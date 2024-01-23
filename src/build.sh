05606c871e737cb26aaba6cba1c73a8e9dff6313
Pawel Cioch <pawel@greenhex.net>
2024-01-22 22:43:15 -0600

    pass the version from build.sh because dynamic variables in Dockerfile impossible

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build -t zcashd . --no-cache
+GIT_TAG=$(curl -sL https://api.github.com/repos/zcash/zcash/releases/latest | jq -r ".tag_name")
+echo "building version $GIT_TAG" 
+docker build -t zcashd --build-arg GIT_TAG=$GIT_TAG .
 
